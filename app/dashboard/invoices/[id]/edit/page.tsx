4432a516be55afee7713a5d638073964c94e3d86
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2024-03-26 14:55:26 -0300

    chapter16-adding-metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoices'
+}
+ 
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
